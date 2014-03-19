25285d96c202db6e4f547c8026166b47b0de35ad
Hugo Tunius <hugo.tunius@gmail.com>
2014-03-19 23:46:39 +0100

    Initial commit

---
+++ LayoutAlternatives/HTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HTAppDelegate.h
+//  LayoutAlternatives
+//
+//  Created by Hugo Tunius on 19/03/14.
+//  Copyright (c) 2014 Hugo Tunius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.0'
+
+pod 'KeepLayout', '~> 1.2.2'
+pod 'Masonry', '~> 0.4.0'
+pod 'UIView+AutoLayout'
