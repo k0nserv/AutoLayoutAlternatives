9708fdfec5058d3871e8a18d449a1306f8f15b65
Hugo Tunius <hugo.tunius@gmail.com>
2014-07-02 15:49:44 +0200

    Initial implementation for keep layout, not working with rotation to landscape

--- LayoutAlternatives/Views/HTKeepLayoutView.m
+++ LayoutAlternatives/Views/HTKeepLayoutView.m
@@ -8,6 +8,29 @@
 
 #import "HTKeepLayoutView.h"
 
+#import <KeepLayout/KeepLayout.h>
+
 @implementation HTKeepLayoutView
+
+- (void)setup {
+    UIView *first = self.views[0], *second = self.views[1], *third = self.views[2], *fourth = self.views[3];
+
+    CGSize size = CGSizeMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
+    for (UIView *v in self.views) {
+        [v keepSize:size];
+    }
+
+    first.keepBottomAlignTo(self).equal = KeepRequired(0.0);
+    first.keepRightAlignTo(self).equal = KeepRequired(0.0);
+
+    second.keepLeftAlignTo(self).equal = KeepRequired(0.0);
+    second.keepBottomAlignTo(self).equal = KeepRequired(0.0);
+
+    third.keepTopAlignTo(self).equal = KeepRequired(0.0);
+    third.keepRightAlignTo(self).equal = KeepRequired(0.0);
+
+    fourth.keepTopAlignTo(self).equal = KeepRequired(0.0);
+    fourth.keepLeftAlignTo(self).equal = KeepRequired(0.0);
+}
 
 @end
